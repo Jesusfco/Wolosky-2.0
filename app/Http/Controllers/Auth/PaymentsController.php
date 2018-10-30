a2015980c467400add4bf5d356ae707abf803d2d
Jesús Fco Cortés <jfcr@live.com>
2018-10-30 15:02:56 -0600

    Update app/Http/Controllers/Auth/PaymentsController.php

--- app/Http/Controllers/Auth/PaymentsController.php
+++ app/Http/Controllers/Auth/PaymentsController.php
@@ -18,7 +18,7 @@ class PaymentsController extends Controller
 
     public function dataToProcess(Request $request) {
 
-        $records = Record::whereBetween('date', [$request->from, $request->to])->get();
+        $records = Record::whereBetween('date', [$request->from, $request->to])->orderBy('date', 'ASC')->get();
         $users = User::whereBetween('user_type_id',[2,5])->where('status', 1)->with(['salary', 'schedules'])->get();
 
         return response()->json(['users' => $users, 'records' => $records ]);
@@ -27,27 +27,27 @@ class PaymentsController extends Controller
 
     public function insertRecords() {
 
-        for($i = 0; $i < 15; $i++) {
+        // for($i = 0; $i < 15; $i++) {
 
-            $record = new Record();
-            $record->user_id = 137;
-            $record->date = "2018-10-" . ($i + 1);
-            $record->checkIn = "01:00:00";
-            $record->checkOut = "23:00:00";
-            $record->save();
-            
-        }
+        //     $record = new Record();
+        //     $record->user_id = 137;
+        //     $record->date = "2018-10-" . ($i + 1);
+        //     $record->checkIn = "01:00:00";
+        //     $record->checkOut = "23:00:00";
+        //     $record->save();
 
-        for($i = 0; $i < 15; $i++) {
+        // }
 
-            $record = new Record();
-            $record->user_id = 136;
-            $record->date = "2018-10-" . ($i + 1);
-            $record->checkIn = "01:00:00";
-            $record->checkOut = "23:00:00";
-            $record->save();
+        // for($i = 0; $i < 15; $i++) {
 
-        }
+        //     $record = new Record();
+        //     $record->user_id = 136;
+        //     $record->date = "2018-10-" . ($i + 1);
+        //     $record->checkIn = "01:00:00";
+        //     $record->checkOut = "23:00:00";
+        //     $record->save();
+
+        // }
     }
 
 
