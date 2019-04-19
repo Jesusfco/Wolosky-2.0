4509bca776c81522994d6364d8bd906d5d45ea5b
Jesús Fco Cortés <jfcr@live.com>
2019-04-19 11:12:10 -0500

    Better cors for hostgator

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -14,11 +14,12 @@ class Cors
      * @return mixed
      */
     public function handle($request, Closure $next)
-    {
+    {        
+
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-XSRF-TOKEN, Authorization')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-XSRF-TOKEN, Authorization');
+            // ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-XSRF-TOKEN, Authorization');
+            ->header('Access-Control-Allow-Headers', '*');
     }
 }
