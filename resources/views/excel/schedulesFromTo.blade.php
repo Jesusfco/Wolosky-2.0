f8715e570c49eaa31cdfbf379c43064804670d51
Jesús Fco Cortés <jfcr@live.com>
2019-09-27 12:48:45 -0500

    Update schedulesFromTo.blade.php

--- resources/views/excel/schedulesFromTo.blade.php
+++ resources/views/excel/schedulesFromTo.blade.php
@@ -10,32 +10,26 @@
     <tr>
         <th>Horarios de {{ $data['from'] . ' - ' . $data['to']}}</th>
     </tr>
+    <table>
+        <tr>
+            @foreach($data['data'] as $day)                             
+                <th>{{$day['day_name']}}</th>                            
+            @endforeach
+        </tr>                    
 
+        @foreach($data['names'] as $users) 
+        <tr>                                                    
+            @foreach($users as $user)
+                @if($user != NULL)
+                    <td>{{ $user['user_name'] }}</td>
+                @else
+                    <td>---------------</td>
+                @endif
+            @endforeach
+        </tr>
+        @endforeach
+    </table>                                   
+                
     
-           
-
-                <table>
-                    <tr>
-                        @foreach($data['data'] as $day)                             
-                            <th>{{$day['day_name']}}</th>                            
-                        @endforeach
-                    </tr>                    
-
-                    @foreach($data['names'] as $users) 
-                    <tr>                                                    
-                        @foreach($users as $user)
-                            @if($user != NULL)
-                                <td>{{ $user }}</td>
-                            @else
-                                <td>hola</td>
-                            @endif
-                        @endforeach
-                    </tr>
-                    @endforeach
-                </table>   
-                
-                {{-- <tr>{{$data['names']}} --}}
-                
-
 </body>
 </html>
