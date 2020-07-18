52e40c86de848ea9fcd63f8becb4e403acd285a4
Jesús Fco Rodriguez <jfcr@live.com>
2020-07-17 19:49:03 -0500

    Update equipo.blade.php

--- resources/views/home/equipo.blade.php
+++ resources/views/home/equipo.blade.php
@@ -16,64 +16,6 @@
 
       @endforeach
 
-   
-
-{{-- <li>
-        <img src="images/equipo/moi.jpg"> 
-        <div class="caption left-align">
-          <h3 class="oswaldo">Led. Moisés Marín</h3>
-          <h5 class="light grey-text text-lighten-3 thin">"Entrenador representante de Academia de Gimnasia Wolosky"</h5>
-        </div>
-      </li> --}}
-
-      <li>
-        <img src="images/equipo/diana.jpg"> 
-        <div class="caption left-align">
-          <h3 class="oswaldo">Led. Diana Lizeth Archila Castañón</h3>
-          <h5 class="light grey-text text-lighten-3 thin">“Me gusta porque es un deporte que implica mucha disciplina, mucha constancia, así como mucha dedicación para poder vencer tus miedos.”</h5>
-        </div>
-      </li>
-
-{{--<li>--}}
-    {{--<img src="images/equipo/sr.jpg"> <!-- random image -->--}}
-    {{--<div class="caption left-align">--}}
-      {{--<h3 class="oswaldo">Lic. Jose Raul Morales Morales</h3>--}}
-      {{--<h5 class="light grey-text text-lighten-3 thin">"Soy entrenador de Gimnasia, lo que gusta de la gimnasia es la disciplina con la que se tiene que aplicar, tiene que ser exacta y lo bonito es que un ejercicio bien hecho es estético, elegante..."</h5>--}}
-    {{--</div>--}}
-  {{--</li>--}}
-
-
-{{-- <li>
-        <img src="images/equipo/humberto.jpg"> <!-- random image -->
-        <div class="caption left-align">
-            <h3 class="oswaldo">Lefd. Jose Humberto Reyes</h3>
-            <h5 class="light grey-text text-lighten-3 thin">"La gimnasia implica retos diarios, requiere mucha fuerza y mucho control..."</h5>
-        </div>
-    </li> --}}
-
-    {{-- <li>
-      <img src="images/equipo/elmer.jpg"> <!-- random image -->
-      <div class="caption left-align">
-          <h3 class="oswaldo">Lefd. Elmer de Jesús</h3>
-          <h5 class="light grey-text text-lighten-3 thin">“Me gusta porque es un deporte que implica mucha disciplina, mucha constancia, así como mucha dedicación para poder vencer tus miedos.”</h5>
-      </div>
-  </li> --}}
-
-  {{-- <li>
-    <img src="images/equipo/ever.jpg"> <!-- random image -->
-    <div class="caption left-align">
-        <h3 class="oswaldo">Lefd. Everado Gumeta</h3>
-        <h5 class="light grey-text text-lighten-3 thin">"Aunque la gimnasia es un deporte de competencia, yo lo veo mas enfocado a una constante competencia con uno mismo"</h5>
-    </div>
-</li> --}}
-
-       {{--<li>--}}
-        {{--<img src="images/equipo/Jose.jpg"> <!-- random image -->--}}
-        {{--<div class="caption left-align">--}}
-          {{--<h3 class="oswaldo">Lefd. Jose Alfonso Esponda</h3>--}}
-          {{--<h5 class="light grey-text text-lighten-3">"Llevo 20 años de entrenador de Gimnasia Artistica, me enfoque en la carrera de entrenamiento deportivo"</h5>--}}
-        {{--</div>--}}
-      {{--</li>--}}
 
     </ul>
   </div>
